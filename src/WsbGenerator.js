e759d89e3d48a5c249eb8a01298daf0ff7b2aa54
moozuknet <moozuknet@naver.com>
2026-08-26 02:54:27 +0900

    fix: Fix XML escaping logic so LogonCommand quotes and URLs are not corrupted in .wsb files

--- src/WsbGenerator.js
+++ src/WsbGenerator.js
@@ -8,13 +8,11 @@ var WsbGenerator = (function() {
    */
   function escapeXml(unsafe) {
     if (!unsafe) return '';
-    return String(unsafe).replace(/[<>&'"]/g, function(c) {
+    return String(unsafe).replace(/[<>&]/g, function(c) {
       switch (c) {
         case '<': return '&lt;';
         case '>': return '&gt;';
         case '&': return '&amp;';
-        case '\'': return '&apos;';
-        case '"': return '&quot;';
         default: return c;
       }
     });
